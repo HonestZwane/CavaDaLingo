c828bd61593d00bf549cb5fd4d7400e4afc29c14
Ntobeko Zwane <ntobeko96zwane@gmail.com>
2019-01-04 01:23:12 +0200

    Stopped on video 17 of Multiscreen Apps

---
+++ java/com/example/ntobekozwane/lingo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ntobekozwane.lingo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void NumbersAct(View view)
+    {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+    public void PhrasesAct(View view)
+    {
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
+    }
+    public void FamilyAct(View view)
+    {
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+    public void ColorsAct(View view)
+    {
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+}
