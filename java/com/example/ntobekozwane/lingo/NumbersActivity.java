263417ad3d466d5ed6a7476ecb02b811a9b59b16
Ntobeko Zwane <ntobeko96zwane@gmail.com>
2019-01-10 00:00:39 +0200

    Stopped on video 66 of Multiscreen Apps. ok officially finished, but need to make it look good

--- java/com/example/ntobekozwane/lingo/NumbersActivity.java
+++ java/com/example/ntobekozwane/lingo/NumbersActivity.java
@@ -1,14 +1,8 @@
 package com.example.ntobekozwane.lingo;
 
-import android.provider.UserDictionary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,7 +11,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<>();
         words.add(new Word("one", "kunye"));
